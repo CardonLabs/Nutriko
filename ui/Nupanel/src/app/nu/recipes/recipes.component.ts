aa9c9632f777441fbb17294712bdd9220b919b38
Romer <romerve@gmail.com>
2020-12-20 14:15:26 -0600

    table displays recipes and expanded row. relates to #9

--- ui/Nupanel/src/app/nu/recipes/recipes.component.ts
+++ ui/Nupanel/src/app/nu/recipes/recipes.component.ts
@@ -1,38 +1,51 @@
-import { Component, AfterViewInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource}  from '@angular/material/table';
-import { RECIPES } from 'src/app/mock/datamock';
-import { IMRecipe } from 'src/app/models/recipes';
+import { MockingService } from 'src/app/mock/mocking.service';
+import { Recipe, RecipeBasic } from 'src/app/models/recipes';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
-  styleUrls: ['./recipes.component.scss']
+  styleUrls: ['./recipes.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ]
 })
-export class RecipesComponent implements AfterViewInit {
+
+export class RecipesComponent implements OnInit {
 
   displayedColumns: string[] = ['name', 'cusine', 'rating', 'popularity', 'calories', 'carbs'];
-  dataSource = new MatTableDataSource<IMRecipe>(RECIPES);
-  //dataSource = RECIPES;
+  recipesDataSource = new MatTableDataSource<Recipe>();
+  recipeData: Array<Recipe>;
+  mockService$: Observable<RecipeBasic[]>;
 
-  @ViewChild(MatPaginator) set matPaginator(paginator: MatPaginator) {
-      this.dataSource.paginator = paginator;
-  }
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
-  constructor() { }
+  constructor(private MockingService: MockingService) { }
 
-  ngAfterViewInit() {
+  ngOnInit() {
+    this.MockingService.getRecipes().subscribe( response => {
+      console.log('full');
+      console.log(response);
+      this.recipesDataSource.data = response;
+      this.recipesDataSource.paginator = this.paginator;
 
-
-    console.log(this.dataSource);
-    //console.log(this.paginator);
+    });
   }
 
   applyFilter(event: Event) {
     
     const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
+    this.recipesDataSource.filter = filterValue.trim().toLowerCase();
     
   }
+
 
 }

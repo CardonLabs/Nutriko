72ab379805dd50694c941fcaaee0ba3af3e60e79
Romer <romerve@gmail.com>
2020-12-26 14:04:06 -0600

    paginator working  - relates to #9

--- ui/Nupanel/src/app/nu/recipes/recipe-list/recipe-list.component.ts
+++ ui/Nupanel/src/app/nu/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ViewChild, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, OnChanges, SimpleChanges, AfterViewInit } from '@angular/core';
 import { MatTableDataSource}  from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { RecipeService } from 'src/app/nu/recipes/recipe.service';
@@ -10,7 +10,7 @@ import { Recipe } from 'src/app/models/recipes';
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.scss']
 })
-export class RecipeListComponent implements OnInit, OnChanges {
+export class RecipeListComponent implements OnInit, OnChanges, AfterViewInit {
 
   @Input() recipeList: Array<Recipe>;
   @ViewChild(MatPaginator) paginator: MatPaginator;
@@ -22,12 +22,15 @@ export class RecipeListComponent implements OnInit, OnChanges {
 
   ngOnInit() {
     this.recipeTableDataSource.data = this.recipeList;
-    this.recipeTableDataSource.paginator = this.paginator;
     console.log(this.recipeTableDataSource.paginator);
   }
 
   ngOnChanges(changes: SimpleChanges) {
     this.recipeTableDataSource.data = this.recipeList;
+  }
+
+  ngAfterViewInit() {
+    this.recipeTableDataSource.paginator = this.paginator;
   }
 
   applyFilter(event: Event) {
